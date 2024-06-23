c5c00fc7113918150569fedf24ee908c08edbc12
Abraham <abraham.attia.pro@gmail.com>
2024-06-23 12:55:00 +0200

    dans la page d'acceuil supression du header existant
    import et utilisation du composant header
    
    Fixes #4

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import { Link } from 'expo-router';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Header from '@/components/Header/Header';
 
-const HomeScreen = () => {
+const HomeScreen = () => 
+{
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Image source={require('../../assets/images/logo.png')} style={styles.logo} />
-        <View style={styles.nav}>
-          <Link style={styles.navItem} href="/accueil">Accueil</Link>
-          <Link style={styles.navItem} href="/profils">Profils</Link>
-          <TouchableOpacity style={styles.navButton}>
-            <Link style={styles.navButtonText} href="/about">Faire le test</Link>
-          </TouchableOpacity>
-        </View>
-      </View>
+
+      <Header />
+
       <View style={styles.mainContent}>
         <Text style={styles.mainText}>Repérez vos besoins, on s’occupe du reste, avec les meilleurs talents</Text>
         <TouchableOpacity style={styles.mainButton}>
@@ -28,43 +22,8 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff', // Couleur de fond claire
     alignItems: 'center',
-  },
-  header: {
-    width: '100%',
-    paddingHorizontal: 20,
-    paddingTop: 40,
-    paddingBottom: 5,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-  },
-  logo: {
-    marginRight: 30,
-    width: 90,
-    height: 30,
-  },
-  nav: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  navItem: {
-    marginHorizontal: 10,
-    fontSize: 16,
-    color: '#333',
-  },
-  navButton: {
-    backgroundColor: '#6200ea',
-    paddingVertical: 8,
-    paddingHorizontal: 15,
-    borderRadius: 5,
-  },
-  navButtonText: {
-    color: '#fff',
-    fontSize: 16,
   },
   mainContent: {
     flex: 1,
@@ -76,6 +35,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     textAlign: 'center',
     marginBottom: 20,
+    color: '#333', // Couleur de texte sombre pour le contraste
   },
   mainButton: {
     backgroundColor: '#6200ea',
